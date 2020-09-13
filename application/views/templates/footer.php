91d4c595ee927869df3143996fcf3cf687abc1b5
Junaid S. Shaikh <junaid96shaikh@gmail.com>
2020-09-13 10:49:32 +0530

    Initial commit

---
+++ application/views/templates/footer.php
@@ -0,0 +1,13 @@
+
+  <footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Version</b> 0.1.0
+    </div>
+    <strong>Copyright &copy; <?php echo date('Y'); ?>.</strong> All rights
+    reserved.
+  </footer>
+</div>
+<!-- ./wrapper -->
+
+</body>
+</html>
